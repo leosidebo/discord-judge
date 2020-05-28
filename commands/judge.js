ea8e3424820a09be300ff8b40660bc955545db9d
Leo Sidebo <abletoirper@gmail.com>
2020-05-28 18:47:28 +0200

    Added input validation and template

--- commands/judge.js
+++ commands/judge.js
@@ -1,7 +1,15 @@
 module.exports = {
     name: 'judge',
+    template: '!judge [subject]',
     description: 'Judge a user',
     execute(message, args) {
+
+        if(args[0] === undefined) {
+            return message.reply('The Judge needs someone to judge!');
+        } else if(args.length > 1) {
+            return message.reply('The Judge cannot judge several people at once!');
+        }
+
         const links = require('../imageLinks.json').links;
         const linkToSend = randomNumber(links.length);
 
